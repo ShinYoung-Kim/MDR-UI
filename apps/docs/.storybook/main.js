0f1fa7d7428ac17d01de0a3070829f498f402ba4
ShinYoung-Kim <lukey0515@naver.com>
2024-12-03 18:28:58 +0900

    chore: 스토리북 설정

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -5,7 +5,7 @@ function getAbsolutePath(value) {
 }
 
 const config = {
-  stories: ["../stories/*.stories.tsx", "../stories/**/*.stories.tsx"],
+  stories: ["../../../packages/ui/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
@@ -17,21 +17,21 @@ const config = {
 
   core: {},
 
-  async viteFinal(config, { configType }) {
-    // customize the Vite config here
-    return {
-      ...config,
-      define: { "process.env": {} },
-      resolve: {
-        alias: [
-          {
-            find: "ui",
-            replacement: resolve(__dirname, "../../../packages/ui/"),
-          },
-        ],
-      },
-    };
-  },
+  // async viteFinal(config, { configType }) {
+  //   // customize the Vite config here
+  //   return {
+  //     ...config,
+  //     define: { "process.env": {} },
+  //     resolve: {
+  //       alias: [
+  //         {
+  //           find: "ui",
+  //           replacement: resolve(__dirname, "../../../packages/ui/"),
+  //         },
+  //       ],
+  //     },
+  //   };
+  // },
 
   docs: {
     autodocs: true,
