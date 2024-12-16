c7fceea1a76007cad7f6cef896df5426feee1544
ShinYoung-Kim <lukey0515@naver.com>
2024-12-16 21:55:57 +0900

    feat: toast position 기능추가

--- packages/ui/toast/src/Toaster.tsx
+++ packages/ui/toast/src/Toaster.tsx
@@ -1,13 +1,31 @@
 import { forwardRef } from "react";
 import { useToaster } from "./useToaster";
 import * as Toast from "./Toast";
+import { toasterStyle } from "./style";
 
-type ToasterProps = {};
+type Position =
+  | "top-left"
+  | "top-center"
+  | "top-right"
+  | "bottom-left"
+  | "bottom-center"
+  | "bottom-right"
+  | "middle-left"
+  | "middle-center"
+  | "middle-right";
+
+type ToasterProps = {
+  maxCount?: number;
+  position?: Position;
+};
 
 export const Toaster = forwardRef<HTMLDivElement, ToasterProps>((props, ref) => {
   const { toastList } = useToaster();
+  const { maxCount = 5, position = "bottom-right" } = props;
+  const style = toasterStyle({ position });
+
   return (
-    <section ref={ref}>
+    <section ref={ref} className={style}>
       {toastList.map(({ id, title, description }) => {
         return (
           <Toast.Root key={id}>
---
+++ packages/ui/toast/src/style.ts
@@ -0,0 +1,17 @@
+import { cva } from "class-variance-authority";
+
+export const toasterStyle = cva("fixed flex flex-col gap-5", {
+  variants: {
+    position: {
+      "top-left": "top-5 left-5",
+      "top-center": "top-5 left-1/2 -translate-x-1/2",
+      "top-right": "top-5 right-5",
+      "middle-left": "top-1/2 -translate-y-1/2 left-5",
+      "middle-center": "top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2",
+      "middle-right": "top-1/2 -translate-y-1/2 right-5",
+      "bottom-left": "bottom-5 left-5",
+      "bottom-center": "bottom-5 left-1/2 -translate-x-1/2",
+      "bottom-right": "bottom-5 right-5",
+    },
+  },
+});
